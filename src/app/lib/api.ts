14695c66331b365f43c3d4890128c021f4294026
Zainab Irfan <zainabirfan12r@gmail.com>
2026-05-22 19:17:22 +0500

    Connected frontend to backend

--- src/app/lib/api.ts
+++ src/app/lib/api.ts
@@ -1,6 +1,6 @@
 // src/app/lib/api.ts
 
-const API_BASE = 'http://127.0.0.1:8000';
+const API_BASE = 'https://prismai-backend-3hsi.onrender.com/';
 
 const getToken = () =>
   typeof window !== 'undefined' ? localStorage.getItem('token') : null;
